7d54e8ea044d853db1f5cbb9febf0be495800a8c
Fernando Lozano <lozanoresino@gmail.com>
2020-12-05 22:17:15 +0100

    registry-service added

--- config-service/src/main/java/com/academy/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/academy/config/ConfigServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
